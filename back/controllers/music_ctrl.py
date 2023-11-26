3a65b39769432110c5f92c2243ed3053b34b171d
andrewkc <andresjkcc@gmail.com>
2023-11-26 11:38:15 -0500

    all

--- back/controllers/music_ctrl.py
+++ back/controllers/music_ctrl.py
@@ -6,36 +6,14 @@ import pandas as pd
 import os
 
 PATH_TO_MUSIC = 'music/songs'  # actualiza con la ruta global
-df = pd.read_csv('music/music_map.csv')
 
 async def get_mp3_by_id(id: str):
     try:
-        song_name = df.loc[['music_id'] == id, 'music_name'].iloc[0]
-
-        """
-        # Decodificar el nombre del archivo de la URL
-        song_name_decoded = unquote(song_name)
-
-        # Normalizar los caracteres Unicode en el nombre del archivo
-        song_name_normalized = unicodedata.normalize('NFKD', song_name_decoded)
-
-        # Encontrar el archivo MP3 correspondiente al nombre de la canción
-        song_file = None
-        for f in os.listdir(PATH_TO_MUSIC):
-            f_normalized = unicodedata.normalize('NFKD', f)
-            if f_normalized.lower().startswith(song_name_normalized.lower()) and f_normalized.endswith('.mp3'):
-                song_file = f
-                break
-
-        if not song_file:
-            raise HTTPException(status_code=404, detail="Song not found")
-        """
-
         # Construir la ruta completa del archivo
-        song_path = os.path.join(PATH_TO_MUSIC, song_name)
+        song_path = os.path.join(PATH_TO_MUSIC, id)
 
         # Retornar el archivo MP3 como una respuesta de transmisión
-        return StreamingResponse(open(song_path, "rb"), media_type="audio/mpeg", headers={"Content-Disposition": f'attachment; filename="{quote(song_name)}"'})
+        return StreamingResponse(open(song_path, "rb"), media_type="audio/mpeg", headers={"Content-Disposition": f'attachment; filename="{quote(id)}"'})
     
     except Exception as e:
         raise HTTPException(status_code=500, detail="Song not found")
